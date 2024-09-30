99ca9f3ee6c364b14650004a39bca671fddcc580
Jasmine Yuen <jyuen30@stuy.edu>
2024-09-30 19:13:01 -0400

    mantine navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
 import "@mantine/core/styles.css";
-import { Center, Code, MantineProvider, Blockquote } from "@mantine/core";
+import {
+  Center,
+  Code,
+  MantineProvider,
+  Blockquote,
+  Flex,
+  AppShell,
+} from "@mantine/core";
 import { theme } from "./theme";
 
 export default function App() {
   return (
     <MantineProvider theme={theme}>
-      <Center h="100vh">
-        <Blockquote color="yellow">
-          Edit <Code>src/App.tsx</Code> to get started.
-        </Blockquote>
-      </Center>
+      <AppShell
+        navbar={{
+          width: 300,
+          breakpoint: "sm",
+          collapsed: { mobile: !open },
+        }}
+      >
+        <AppShell.Navbar>
+          <Flex direction="row">navbar</Flex>
+        </AppShell.Navbar>
+        <Center h="100vh">
+          <Blockquote color="yellow">
+            Edit <Code>src/App.tsx</Code> to get started.
+          </Blockquote>
+        </Center>
+      </AppShell>
     </MantineProvider>
   );
 }
