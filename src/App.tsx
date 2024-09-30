d9755bb1f20118c48c68ba834057e3659ee220a2
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2024-09-30 17:34:30 -0400

    make hytech gold the primary mantine color :)

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <MantineProvider theme={theme}>
       <Center h="100vh">
-        <Blockquote color="yellow">
+        <Blockquote>
           Edit <Code>src/App.tsx</Code> to get started.
         </Blockquote>
       </Center>
--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,20 @@
 import { createTheme } from "@mantine/core";
 
 export const theme = createTheme({
-  /* Put your mantine theme override here */
+  primaryColor: "hytech-gold",
+  primaryShade: { light: 5, dark: 5 },
+  colors: {
+    "hytech-gold": [
+      "#fbf7e6",
+      "#f0ecda",
+      "#ded7ba",
+      "#cbc097",
+      "#bbad79",
+      "#b3a369",
+      "#ad9b5a",
+      "#97874a",
+      "#86773e",
+      "#746730",
+    ],
+  },
 });
