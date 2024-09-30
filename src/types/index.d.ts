77cbc79681682f6f63a8c87cb3d86f3e7a74be8f
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2024-09-30 17:46:16 -0400

    wip datatable component

---
+++ src/types/index.d.ts
@@ -0,0 +1,14 @@
+type MCAPFileInformation = {
+  id: string;
+  mcap_file_name: string;
+  matlab_file_name: string;
+  aws_bucket: string;
+  mcap_path: string;
+  mat_path: string;
+  vn_lat_lon_path: string;
+  velocity_plot_path: string;
+  date: string;
+  location: string;
+  notes: string?;
+  event_type: string?;
+};
