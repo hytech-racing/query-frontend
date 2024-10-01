425407266347632f629330e929b1851b2851568b
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2024-10-01 13:41:05 -0400

    make table scrollable and display text on initial + no results state

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -3,43 +3,59 @@ import { Table } from "@mantine/core";
 import { useMantineTheme } from "@mantine/core";
 
 interface DataTableProps {
-  data: MCAPFileInformation[];
+  data?: MCAPFileInformation[];
 }
 
 export default function DataTable({ data }: DataTableProps) {
   const theme = useMantineTheme();
   const [selectedRow, setSelectedRow] = useState<string>();
 
-  const rows = data.map((file) => (
-    <Table.Tr
-      key={file.id}
-      onClick={() => setSelectedRow(file.id)}
-      fw={selectedRow == file.id ? "bold" : ""}
-      bg={selectedRow == file.id ? theme.primaryColor : ""}
-    >
-      <Table.Td>{file.mcap_file_name}</Table.Td>
-      <Table.Td>{file.date}</Table.Td>
-      <Table.Td>{file.location}</Table.Td>
-      <Table.Td>{file.notes}</Table.Td>
+  const rows = !data ? (
+    <Table.Tr>
+      <Table.Td colSpan={100} ta="center">
+        Use the filters to get results
+      </Table.Td>
     </Table.Tr>
-  ));
+  ) : data.length === 0 ? (
+    <Table.Tr>
+      <Table.Td colSpan={100} ta="center">
+        No results found
+      </Table.Td>
+    </Table.Tr>
+  ) : (
+    data.map((file) => (
+      <Table.Tr
+        key={file.id}
+        onClick={() => setSelectedRow(file.id)}
+        fw={selectedRow === file.id ? "bold" : ""}
+        bg={selectedRow === file.id ? theme.primaryColor : ""}
+      >
+        <Table.Td>{file.mcap_file_name}</Table.Td>
+        <Table.Td>{file.date}</Table.Td>
+        <Table.Td>{file.location}</Table.Td>
+        <Table.Td>{file.notes}</Table.Td>
+      </Table.Tr>
+    ))
+  );
 
   return (
-    <Table
-      stickyHeader
-      striped
-      highlightOnHover
-      highlightOnHoverColor={theme.primaryColor}
-    >
-      <Table.Thead bg={theme.primaryColor}>
-        <Table.Tr>
-          <Table.Th>Name</Table.Th>
-          <Table.Th>Date</Table.Th>
-          <Table.Th>Location</Table.Th>
-          <Table.Th>Notes</Table.Th>
-        </Table.Tr>
-      </Table.Thead>
-      <Table.Tbody>{rows}</Table.Tbody>
-    </Table>
+    <Table.ScrollContainer h="100%" minWidth={800}>
+      <Table
+        stickyHeader
+        striped
+        highlightOnHover={data && data.length > 0}
+        highlightOnHoverColor={theme.primaryColor}
+      >
+        <Table.Thead bg={theme.primaryColor}>
+          <Table.Tr>
+            <Table.Th>Name</Table.Th>
+            <Table.Th>Date</Table.Th>
+            <Table.Th>Location</Table.Th>
+            <Table.Th>Notes</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>{rows}</Table.Tbody>
+      </Table>
+    </Table.ScrollContainer>
   );
 }
