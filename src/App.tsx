909c72f3cd83ee29ffb928d67a534ccbd623a20f
Christine <132790780+aesteri@users.noreply.github.com>
2024-10-06 18:36:50 +0900

    integrated checkpoints

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,20 @@ import { theme } from "@/theme";
 import Navbar from "@/Navbar.tsx";
 import SearchBar from "@/components/SearchBar";
 import "@/css/App.css";
-import DataTable from "@/components/DataTable";
-import PreviewCard from "@/components/PreviewCard";;
 
 
 export default function App() {
   return (
     <MantineProvider theme={theme}>
       <div className="app-container">
-        {/* Navbar at the top */}
         <header className="navbar">
           <Navbar />
         </header>
 
-        <div className="column-content">
-          {/* Two column layout for DataTable and an empty component */}
-          <div className="main-content">
-            <SearchBar />
-          </div>
+        <div className="main-content">
+          <SearchBar />
         </div>
+
         <footer className="footer">
             <h3>made by Frontend team 2024</h3>
           </footer>
