2e155f07fc62f81defc0df7c5d912bbe9a79f71a
Cyra Alesha <cyraanindyaalesha@gmail.com>
2025-01-18 08:03:39 -0500

    Doc Version below navbar

--- src/components/DocumentationVersion.tsx
+++ src/components/DocumentationVersion.tsx
@@ -1,22 +1,24 @@
 import React from "react";
-import { Select } from '@mantine/core';
+import { Select } from "@mantine/core";
+import "@/css/DocumentationVersion.css";
 
 
-interface DocumentationVersion {
-    versions: string[];
+interface DocumentationVersionProps {
+  versions: string[];
 }
 
-const DocumentationVersion: React.FC<DocumentationVersion> = ({ versions }) => {
-    return (
-        <div className="documentationversion">
-            <Select
-                label="Version"
-                placeholder="Select Documentation version"
-                data={[1, 2, 3, 4, 5]} //{versions}
-                size="xs"
-            />
-        </div>
-    );
-}
+const DocumentationVersion: React.FC<DocumentationVersionProps> = ({ versions }) => {
+  return (
+    <div className="documentationversion">
+      <Select
+        label="Documentation Version"
+        placeholder="Select Documentation version"
+        data={versions.map((version) => ({ value: version, label: version }))}
+        size="xs"
+        className="documentationselect"
+      />
+    </div>
+  );
+};
 
 export default DocumentationVersion;
--- src/routes/docs.tsx
+++ src/routes/docs.tsx
@@ -1,4 +1,7 @@
 import DocumentationVersion from "@/components/DocumentationVersion";
+
 export default function Docs() {
-  return <DocumentationVersion />;
+  const versions = ["1.0", "1.1", "2.0", "2.1"]; // Example versions, replace with API later.
+
+  return <DocumentationVersion versions={versions} />;
 }
