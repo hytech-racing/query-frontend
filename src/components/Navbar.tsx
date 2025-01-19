73beb23c36cf3b78916aacbf1e498f2b4368ec18
Cyra Alesha <cyraanindyaalesha@gmail.com>
2025-01-19 12:46:39 -0500

    Add documentation selector and displayer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,6 @@ import "@mantine/core/styles.css";
 import "@/css/Navbar.css";
 import { NavLink, useLocation } from "react-router-dom";
 import FileUpload from "@/components/FileUpload"
-import DocumentationVersion from "@/components/DocumentationVersion";
 
 const mainLinksData = [
   { name: "Files", url: "/" },
@@ -13,17 +12,11 @@ const mainLinksData = [
 export default function Navbar() {
   const hytechName = "HyTech Racing Checkpoint 1";
 
-  
   const links = mainLinksData.map(({ name, url }) => (
     <NavLink key={name} className="nav-link" to={url}>
       {name}
     </NavLink>
   ));
-
-
-  //For Docs Version
-  const location = useLocation(); 
-
 
   return (
     <nav id="navbar">
@@ -33,13 +26,7 @@ export default function Navbar() {
         className="navbar-icon"
       />
       
-       
-      {location.pathname === "/docs" && (
-        <DocumentationVersion versions={["1.0", "1.1", "2.0"]} /> // example versions. Replace with API later
-      )}
       {links}
-
-     
 
       <FileUpload uploadUrl={`${import.meta.env.VITE_API_URL}/api/v2/mcaps/bulk_upload`}/>
 
