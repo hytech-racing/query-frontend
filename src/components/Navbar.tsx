ef09140e370aaea4ec955fa0058c70588202249a
Cyra Alesha <cyraanindyaalesha@gmail.com>
2025-01-19 12:18:24 -0500

    Dropdown in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,8 @@
 import "@mantine/core/styles.css";
 import "@/css/Navbar.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import FileUpload from "@/components/FileUpload"
+import DocumentationVersion from "@/components/DocumentationVersion";
 
 const mainLinksData = [
   { name: "Files", url: "/" },
@@ -12,11 +13,17 @@ const mainLinksData = [
 export default function Navbar() {
   const hytechName = "HyTech Racing Checkpoint 1";
 
+  
   const links = mainLinksData.map(({ name, url }) => (
     <NavLink key={name} className="nav-link" to={url}>
       {name}
     </NavLink>
   ));
+
+
+  //For Docs Version
+  const location = useLocation(); 
+
 
   return (
     <nav id="navbar">
@@ -25,7 +32,14 @@ export default function Navbar() {
         alt="Logo"
         className="navbar-icon"
       />
+      
+       
+      {location.pathname === "/docs" && (
+        <DocumentationVersion versions={["1.0", "1.1", "2.0"]} /> // example versions. Replace with API later
+      )}
       {links}
+
+     
 
       <FileUpload uploadUrl={`${import.meta.env.VITE_API_URL}/api/v2/mcaps/bulk_upload`}/>
 
