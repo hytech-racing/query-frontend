fd0e83c75adcd4045c0ebcbf2d2633d2ef23d74e
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2026-04-13 10:38:10 -0400

    add HTX filter

--- src/data/dataFilters.ts
+++ src/data/dataFilters.ts
@@ -6,17 +6,17 @@
  */
 export const eventType: string[] = [
   "Acceleration", // Event type for acceleration tests.
-  "Endurance",    // Event type for endurance tests.
-  "Skidpad",      // Event type for skidpad tests.
-  "Autocross",    // Event type for autocross tests.
+  "Endurance", // Event type for endurance tests.
+  "Skidpad", // Event type for skidpad tests.
+  "Autocross", // Event type for autocross tests.
 ];
 
 /**
  * Represents the list of possible car models.
  *
-  */
+ */
 export const carModel: string[] = [
   "HT08", // 2023-2024 Car
   "HT09", // 2024-2025 Car
-]
-
+  "HTX",
+];
