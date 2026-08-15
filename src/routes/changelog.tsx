b9e9433caf1774bcafd024c724cb1bf58194d460
Sid Arutla <sidarutla@gmail.com>
2026-08-14 20:01:31 -0700

    add changelog placeholder

--- src/routes/changelog.tsx
+++ src/routes/changelog.tsx
@@ -1,6 +1,14 @@
+import { Text } from "@mantine/core";
+
 export default function Changelog() {
   return (
-  <>
-  </>
-  )
+    <div style={{ textAlign: "center", paddingTop: "40px" }}>
+      <Text size="lg" fw={700}>
+        Changelog coming soon
+      </Text>
+      <Text size="sm" c="dimmed">
+        This page isn't populated yet.
+      </Text>
+    </div>
+  );
 }
